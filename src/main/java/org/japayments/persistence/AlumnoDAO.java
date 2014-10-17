66884c0e93cfc3a13d6fe874b8ab11fab2f373f1
joaquinPega <joaquin_pega@hotmail.com>
2014-10-17 15:26:15 -0300

    Watch the todo list

---
+++ src/main/java/org/japayments/persistence/AlumnoDAO.java
@@ -0,0 +1,39 @@
+package org.japayments.persistence;
+
+import java.util.List;
+
+import org.japayments.model.Alumno;
+
+public class AlumnoDAO extends MongoUtil implements GenericDAO<Alumno, String> {
+
+	@Override
+	public void delete(Alumno t) throws DBFail {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(Alumno t) throws DBFail {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Alumno t) throws DBFail {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Alumno> getAll() throws DBFail {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Alumno getByID(String id) throws DBFail {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
